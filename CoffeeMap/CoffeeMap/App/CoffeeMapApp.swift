a05107344e07ad8db6b410bae98bfe0b183615c9
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-10-29 13:53:47 +0300

    created base Xcode project

---
+++ CoffeeMap/CoffeeMap/App/CoffeeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMapApp.swift
+//  CoffeeMap
+//
+//  Created by Khusrav Safiev on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
